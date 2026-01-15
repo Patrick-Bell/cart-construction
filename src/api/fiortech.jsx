80602efcaa39d5aa93f55160efd061a766ef31c9
Patrick Bell <patrick@Patricks-Laptop.local>
2026-01-15 13:23:54 +0000

    first commit

---
+++ src/api/fiortech.jsx
@@ -0,0 +1,57 @@
+import cart1 from '../assets/cart-tracker/cart1.png'
+import cart2 from '../assets/cart-tracker/cart2.png'
+import cart3 from '../assets/cart-tracker/cart3.png'
+import cart4 from '../assets/cart-tracker/cart4.png'
+import cart5 from '../assets/cart-tracker/cart5.png'
+import cart6 from '../assets/cart-tracker/cart6.png'
+import cart7 from '../assets/cart-tracker/cart7.png'
+import cart8 from '../assets/cart-tracker/cart8.png'
+import cart9 from '../assets/cart-tracker/cart9.png'
+import cart10 from '../assets/cart-tracker/cart10.png'
+import cart11 from '../assets/cart-tracker/cart11.png'
+import cart12 from '../assets/cart-tracker/cart12.png'
+import cart13 from '../assets/cart-tracker/cart13.png'
+import cart14 from '../assets/cart-tracker/cart14.png'
+import cart15 from '../assets/cart-tracker/cart15.png'
+import cart16 from '../assets/cart-tracker/cart16.png'
+import cart17 from '../assets/cart-tracker/cart17.png'
+import cart18 from '../assets/cart-tracker/cart18.png'
+import cart19 from '../assets/cart-tracker/cart19.png'
+import cart20 from '../assets/cart-tracker/cart20.png'
+import cart21 from '../assets/cart-tracker/cart21.png'
+import cart22 from '../assets/cart-tracker/cart22.png'
+import cart23 from '../assets/cart-tracker/cart23.png'
+import cart24 from '../assets/cart-tracker/cart24.png'
+import cart25 from '../assets/cart-tracker/cart25.png'
+import cart26 from '../assets/cart-tracker/cart26.png'
+import cart27 from '../assets/cart-tracker/cart27.png'
+
+
+export const cartTrackerSlider = [
+    { title: 'Dashboard Page', desc: 'Main page with summary of key metrics, upcoming events and customisation', image: cart1 },
+    { title: 'Fixtures Page', desc: 'Fixture page where you can view all fixtures, and the next fixture', image: cart2 },
+    { title: 'Analytics Page 1/2', desc: 'View best and worst performers and months', image: cart3 },
+    { title: 'Analytics Page 2/2', desc: 'View monthly breakdowns, with charts', image: cart4 },
+    { title: 'Support Page 1/2', desc: 'Step by step guides', image: cart5 },
+    { title: 'Suport Page 2/2', desc: 'Access to downloadable manuals, upcoming changes and keyboard shortcuts', image: cart6 },
+    { title: 'Profile Page 1/2', desc: 'Users can edit their personal data and change their passwords', image: cart7 },
+    { title: 'Profile Page 2/2', desc: 'Users can toggle notifications', image: cart8 },
+    { title: 'Not Found Page', desc: 'When a page is not found', image: cart9 },
+    { title: 'User Manual', desc: 'Example of user manual contents page (PDF)', image: cart10 },
+    { title: 'Tutorial Mode', desc: 'Automatic tutorial when the user logs on for the first time', image: cart11 },
+    { title: 'Search', desc: 'Powerful search feature that allows you to search for anything on the application', image: cart12 },
+    { title: 'Workers Page', desc: 'View all workers, and create actions such as view, add to watchlist', image: cart13 },
+    { title: 'Workers Page > Worker > Overview', desc: 'Dynamic worker page that shows overall stats', image: cart14 },
+    { title: 'Workers Page > Worker > Game Statistics', desc: 'Overview of a workers game portfolio, includes key metrics', image: cart15 },
+    { title: 'Workers Page > Worker > Analytics', desc: 'Provides more details on performance', image: cart16 },
+    { title: 'Worker PDF', desc: 'Downloadable PDF to show all key metrics for a worker. For example, their work summary', image: cart17 },
+    { title: 'Games Page', desc: 'View all games, and create actions such as view', image: cart18 },
+    { title: 'Add Game Modal', desc: 'Modal to add a game from a selected list. Validation included', image: cart19 },
+    { title: 'Games Page > Game > Overview 1/2', desc: 'Dynamic game page that shows overall metrics such as number of workers, profits, performance and comparisons to previous games', image: cart20 },
+    { title: 'Games Page > Game > Overview 2/2', desc: 'Includes charts and graphs', image: cart21 },
+    { title: 'Games Page > Game > Statistics', desc: 'Table view of all workers and their performance data including sales and profits. Easily navigate to a worker by clicking their name', image: cart22 },
+    { title: 'Filter', desc: 'Filter carts by sales or margin data', image: cart23 },
+    { title: 'Add Cart Modal Tutorial', desc: 'Step by step guide on what each section means', image: cart24 },
+    { title: 'Add Cart Modal Validation', desc: 'Validation in place to prevent carts being added incorrectly', image: cart25 },
+    { title: 'Games Page > Game > Attendance', desc: 'Page that shows all workers, their start and finish time, and how many hours they worked. It also calculates how early/late each worker was and adds it to their profile', image: cart26 },
+]
